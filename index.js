12a072131ce537c924b30dccdc1a404ae299287e
Anurag <hamsen3880@gmail.com>
2024-09-26 18:32:05 +0530

    Server creation V0.1

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
 
-const PORT = 8000
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: 'Backend Signal Incoming',
+        success: true
+    })
+})
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended: true}))
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions))
+
+const PORT = 8368
 
 app.listen(PORT, () => {
     console.log(`Server listening at Port ${PORT}`)
