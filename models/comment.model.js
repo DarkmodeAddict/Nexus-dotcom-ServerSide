7ef5fd4a5e0c08096d9635714f1f83a0a7c770ba
Anurag <hamsen3880@gmail.com>
2024-09-27 00:07:38 +0530

    User Post Comment Model V0.3

---
+++ models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    }
+})
+
+export default commentSchema = mongoose.model('Comment', commentSchema)
